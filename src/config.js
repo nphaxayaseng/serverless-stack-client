600c692b2587c5fdaa775c282dfe54ccc3bc70b9
nphaxayaseng <nphaxayaseng@gmail.com>
2020-10-07 11:29:59 -0700

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "noland-phaxayaseng-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east.1",
+      URL: "https://gykbg9g4x3.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_axFSG3bPZ",
+      APP_CLIENT_ID: "4gdunpnc7mt8vft1o4bmio47rh",
+      IDENTITY_POOL_ID: "us-east-1:c7ad70a7-497b-4084-8382-b813374e8ea6"
+    }
+  };
