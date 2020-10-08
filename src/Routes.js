49120d6c3605c4685aa7f715ae35245b8ca7c0f4
nphaxayaseng <nphaxayaseng@gmail.com>
2020-10-08 13:45:02 -0700

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,38 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <UnauthenticatedRoute path="/login">
+                <Login />
+            </UnauthenticatedRoute>
+            <UnauthenticatedRoute path="/signup">
+                <Signup />
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute path="/settings">
+                <Settings />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute path="/notes/new">
+                <NewNote />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute path="/notes/:id">
+                <Notes />
+            </AuthenticatedRoute>
+            <Route path="/settings">
+                <Settings />
             </Route>
             <Route>
                 <NotFound />
--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51HZK8hIXoHaac9w6Xs8EDqiYfVTYHNw606AAFj7bop9QecbmwTjzX2H0iE0DHfua8WJuZb2Hextur6uGf2DSYHfb00OKwi5ZOY",
     s3: {
       REGION: "us-east-1",
       BUCKET: "noland-phaxayaseng-notes-app-uploads"
     },
     apiGateway: {
-      REGION: "us-east.1",
+      REGION: "us-east-1",
       URL: "https://gykbg9g4x3.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
@@ -13,4 +15,5 @@ export default {
       APP_CLIENT_ID: "4gdunpnc7mt8vft1o4bmio47rh",
       IDENTITY_POOL_ID: "us-east-1:c7ad70a7-497b-4084-8382-b813374e8ea6"
     }
+    
   };
